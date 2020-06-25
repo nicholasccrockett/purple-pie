ac70a3355918e1e502093dd43e343781bea5cdf0
nicholasccrockett <nicholascccrockett@gmail.com>
2020-06-25 07:35:33 -0500

    Added comments for clarity

--- purple_pie_bot.py
+++ purple_pie_bot.py
@@ -4,15 +4,18 @@ import os
 import discord
 from dotenv import load_dotenv
 
+# Loads env variables, mainly the DISCORD_TOKEN
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()
 
+#logs to the console when connection is succesfully made
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
+#On message detect, check if the words "purple pie" are in the message. If they are >> kick the user and delete the purple pie message
 @client.event
 async def on_message(message):
     if message.author == client.user:
